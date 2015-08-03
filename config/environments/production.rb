06c5eb4cde83b223ca941b498f49f20b5c1f5377
André Pinho <andresouzapinho@gmail.com>
2015-08-03 18:35:48 -0300

    Creates and configures contact mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,9 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
+  #todo trocar host pra url de produção
+  config.action_mailer.default_url_options = { host: 'sitelara.com.br' }
+
   # Code is not reloaded between requests.
   config.cache_classes = true
 
